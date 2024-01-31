c0d9866e37fdafde591d87f99289e3cd299cf6b9
ashish784 <mauryaashish784@gmail.com>
2024-02-01 00:38:39 +0530

    change c

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -72,7 +72,7 @@ const HeroBanner = () => {
 
     const intervalId = setInterval(() => {
       setRandomBackground();
-    }, 5000);
+    }, 2000);
 
     return () => clearInterval(intervalId);
   }, [data]);
