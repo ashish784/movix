0ac15412efc3d56187bddce4028d7fefa6d000c0
ashish784 <mauryaashish784@gmail.com>
2024-02-02 01:39:04 +0530

    newpl

--- src/pages/home/nowPlaying/NowPlaying.jsx
+++ src/pages/home/nowPlaying/NowPlaying.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Carousel from '../../../components/carousel/Carousel';
+
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTab/SwitchTabs';
 import useFetch from '../../../hooks/useFetch';
 
+
 const NowPlaying = () => {
-  const [mediaType, setMediaType] = useState("movie");
 
-  const { data, loading } = useFetch(`/${mediaType}/now_playing`);
+    const [endpoint, setEndpoint] = useState("movie");
 
-  const onTabChange = (tab) => {
-    setMediaType(tab === "Movies" ? "movie" : "tv");
-  };
+    const { data, loading } = useFetch(`/${endpoint}/now_playing`);
+  
+    const onTabChange = (tab) => {
+         setEndpoint(tab === "Movies" ? "movie" : "tv");
+    };
 
   return (
     <div className="carouselSection">
-      <ContentWrapper>
-        <span className="carouselTitle">Now Playing</span>
-        <SwitchTabs data={["Movies"]} onTabChange={onTabChange} />
-      </ContentWrapper>
-      <Carousel data={data?.results} loading={loading} endpoint={mediaType} />
+        <ContentWrapper>
+            <span className="carouselTitle">Now Playing</span>
+            <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+        </ContentWrapper>
+        <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
     </div>
   );
 };
