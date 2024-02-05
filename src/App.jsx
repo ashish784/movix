ab4ecc26e32ca0355907d06eba3fea68926fb03f
ashish784 <mauryaashish784@gmail.com>
2024-02-06 02:01:45 +0530

    reload

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     <Header />
      <Routes>
        <Route path="/" element={<Home />}  />
-       <Route path="/:mediaType/:id" element={<Details />}  />
+       <Route path="/details/:mediaType/:id" element={<Details />}  />
        <Route path="/search/:query" element={<SearchResult />}  />
        <Route path="/explore/:mediaType" element={<Explore />}  />
        <Route path="*" element={<PageNotFound />}  />
