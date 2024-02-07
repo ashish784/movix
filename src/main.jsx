8ad96d370d488d14f5598f521fbd0222c1883646
ashish784 <mauryaashish784@gmail.com>
2024-02-07 13:32:13 +0530

    aqo

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import App from './App.jsx';
+// import './index.scss';
+// import { store } from "./store/store";
+
+// ReactDOM.createRoot(document.getElementById('root')).render( 
+//     <Provider store={store}>
+//       <App /> 
+//     </Provider>
+    
+// );
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.scss';
-import { store } from "./store/store";
-Provider
+import { store } from './store/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render( 
-    <Provider store={store}>
-      <App /> 
-    </Provider>
-    
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
+
